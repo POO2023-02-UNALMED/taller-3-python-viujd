c7163b3d18d7c4577d2214771cdefda58d871099
Juan David Robayo <141962352+viujd@users.noreply.github.com>
2023-08-27 19:10:16 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -1,4 +1,29 @@
 class Control:
 
-    def turnOn():
+    def __init__(self):
+        self._tv=None
+
+    def turnOn(self):
+        self._tv.turnOn()
+
+    def turnOff(self):
+        self._tv.turnOff()
+
+    def canalUp(self):
+        self._tv.canalUp()
+    
+    def canalDown(self):
+        self._tv.canalDown()
+
+    def volumenUp(self):
+        self._tv.volumenUp()
+
+    def volumenDown(self):
+        self._tv._volumenDown()
+
+    def setCanal(self, canal):
+        self._tv.setCanal(canal)
+
+    def setVolumen(self, volumen):
+        self._tv.setVolumen(volumen)
         
